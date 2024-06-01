672947e8d010847fcb237f1c4f772cfe8985508e
Olga Pechenkina <Olha.pechyonkina@yandex.ru>
2024-06-01 17:49:54 +0500

    Wov commit

--- src/test/java/ru/netology/CardWithDeliveryTest.java
+++ src/test/java/ru/netology/CardWithDeliveryTest.java
@@ -6,23 +6,23 @@ import org.openqa.selenium.Keys;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class CardWithDiliveryTest {
-    private String generateDate(String pattern){
-        return LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern(pattern));
+public class CardWithDeliveryTest {
+    private String generateDate(long addDays, String pattern){
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
     @Test
     public void shouldCreateCardDelivery(){
         open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Магнитогорск");
-        String planningDate = generateDate("dd.MM.yyyy");
+        $("[data-test-id='city'] input").setValue("Казань");
+        String planningDate = generateDate(4,"dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
-        $("[data-test-id='name'] input").setValue("Печенкина Ольга");
-        $("[data-test-id='phone'] input").setValue("+79517701314");
+        $("[data-test-id='name'] input").setValue("Спириидонов Никита");
+        $("[data-test-id='phone'] input").setValue("+79250561286");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
